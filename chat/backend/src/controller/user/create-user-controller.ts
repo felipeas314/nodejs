bd09334cfc2f4d9d710de410a895a3667183c98d
Felipe Alexandre <felipeas314@gmail.com>
2022-06-18 15:58:36 -0300

    feat: add mongo mongo-express and redis config

---
+++ chat/backend/src/controller/user/create-user-controller.ts
@@ -0,0 +1,9 @@
+import { Request, Response, Send } from "express";
+
+export default class CreateuserController {
+    constructor() {}
+
+    async handle(request: Request, response: Response): Promise<Response> {
+        return response.status(200).json('ok');
+    }
+}
